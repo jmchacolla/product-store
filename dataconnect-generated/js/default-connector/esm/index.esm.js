4ed6407fef28197a2d60459797334bb663568683
jmchacolla <jmchacolla@gmail.com>
2025-05-06 18:29:14 -0400

    Ennable github actions

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'product-store',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'product-store',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
